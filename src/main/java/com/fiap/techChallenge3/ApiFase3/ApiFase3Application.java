91e7fa8fd28b0a23094221fa7d1bcf76c7bb655f
Felipe Santos <felipe.dss17@gmail.com>
2023-09-24 13:22:04 -0300

    commite inicial

---
+++ src/main/java/com/fiap/techChallenge3/ApiFase3/ApiFase3Application.java
@@ -0,0 +1,13 @@
+package com.fiap.techChallenge3.ApiFase3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFase3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFase3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/techChallenge3/ApiFase3/ApiFase3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.techChallenge3.ApiFase3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFase3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
